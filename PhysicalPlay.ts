3e46c8d1522190058d8ed6160fc484dae10ed5fc
Noah Cowit <36167412+nqc1@users.noreply.github.com>
2022-06-13 00:08:21 -0600

    Create PhysicalPlay.ts

---
+++ PhysicalPlay.ts
@@ -0,0 +1,22 @@
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AAAAAA" weight=50
+namespace Apple {
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
